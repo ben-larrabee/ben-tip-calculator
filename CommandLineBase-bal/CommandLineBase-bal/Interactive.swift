ce7388fd7e182263f68dc981b60186c4d72911ef
Ben Larrabee <ben.larrabee@interapthq.com>
2016-10-11 11:09:42 -0400

    finished tip

--- CommandLineBase-bal/CommandLineBase-bal/Interactive.swift
+++ CommandLineBase-bal/CommandLineBase-bal/Interactive.swift
@@ -12,19 +12,26 @@ class Interactive {
   private var done: Bool = false
   private var currentInput: String = "q"
   private var io = Io()
+  private var helper = Helper()
+  private var tipper = Tipper()
+  private var tip : Double = 0.0
   
   func go() {
     
     while !done {
-      
+      showOff()
       // Ask the user for input
-      io.writeMessage("\nInput?")
+      io.writeMessage("\n\nWelcome to Tip Calculator by  Ben Larrabee\nPress 't' to calculate a tip, press 'q' to quit, or press 'h' for help.")
       currentInput = io.getInput()
       
       if currentInput == "q"  {
         done = true
-      } else {
-        print("The input is: \(currentInput)")
+      } else if currentInput == "h" {
+        helper.helpprompt(from: .main)
+        print("I didn't understand: \(currentInput).  Please\nPress 't' to calculate a tip, press 'q' to quit, or press 'h' for help.")//clearly this needs to change
+      } else if currentInput == "t" {
+        tipper.tipprompt()
+        sleep(2)
       }
       
     }
@@ -32,5 +39,13 @@ class Interactive {
     
     return  // end of program
     
+  }
+  func showOff() {
+    print("  ____/\\__ ___________.__         _________        .__               .__          __")
+    print(" /   / /_/ \\__    ___/|__|_____   \\_   ___ \\_____  |  |   ____  __ __|  | _____ _/  |_  ___________")
+    print("  \\_/ / \\    |    |   |  \\____ \\  /    \\  \\/\\__  \\ |  | _/ ___\\|  |  \\  |  \\__  \\   __\\/  _ \\_  __ \\")
+    print(" / / /   \\   |    |   |  |  |_> > \\     \\____/ __ \\|  |_\\  \\___|  |  /  |__/ __ \\|  | (  <_> )  | \\/")
+    print("/_/ /__  /   |____|   |__|   __/   \\______  (____  /____/\\___  >____/|____(____  /__|  \\____/|__|")
+    io.writeMessage("  \\/   \\/                |__|             \\/     \\/          \\/                \\/ (c) Ben Larrabee                  ")
   }
 }
--- CommandLineBase-bal/CommandLineBase-bal/io.swift
+++ CommandLineBase-bal/CommandLineBase-bal/io.swift
@@ -19,8 +19,39 @@ class Io {
       return "Invalid input!"
     }
   }
+  func getDoubleAmount() -> Double {
+    let rawInput = getInput()
+    if rawInput == "q" {
+      return -1
+    } else if rawInput == "h" {
+      return -2
+    } else {
+    let maybeDouble : Double? = Double(rawInput)
+//    var maybeDouble : Double?
+//    let keyboard = FileHandle.standardInput
+//    let inputData = keyboard.availableData
+//    let digs = NSString(data: inputData, encoding: String.Encoding.utf8.rawValue)
+//    maybeDouble = Double(digs)!
+    return maybeDouble!
+    }
+  }
+  
+  func getIntAmount() -> Int {
+    let rawInput = getInput()
+    if rawInput == "q" {
+      return -1
+    } else if rawInput == "h" {
+      return -2
+    } else {
+    let maybeInt : Int? = Int(rawInput)
+    return maybeInt!
+    }
+  }
   
   func writeMessage(_ message: String) {
     print("\(message)\n")
+  }
+  func ioPrint(_ message: String) {
+    print(message)
   }
 }
